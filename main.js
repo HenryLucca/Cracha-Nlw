0f87e3e9c7995f29ab7f91d0a6b7012b7121a5b1
Henry Lucca <66319404+HenryLucca@users.noreply.github.com>
2021-10-24 09:49:08 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const LinksSocialMedia = {
+  github: "HenryLucca",
+  linkedin: "/in/henry-lucca-a7093b220",
+  facebook: "henry.lucca.fg",
+  instagram: "henry_lucca02",
+};
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute("class");
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`;
+  }
+}
+
+changeSocialMediaLinks();
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      userName.textContent = data.name;
+      userBio.textContent = data.bio;
+      userLink.href = data.html_url;
+      UserImage.src = data.avatar_url;
+      userLogin.textContent = data.login;
+    });
+}
+
+getGitHubProfileInfos();
